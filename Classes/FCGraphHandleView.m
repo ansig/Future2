cb69b9105fa8c98f0f75c0c4ca13638e771d1daa
ansig <anders.sigfridsson@gmail.com>
2010-10-27 09:45:21 +0100

    Added gpl license header (issue 2)

--- Classes/FCGraphHandleView.m
+++ Classes/FCGraphHandleView.m
@@ -1,9 +1,28 @@
+/*
+ 
+ TiY (tm) - an adaptable iPhone application for self-management of type 1 diabetes
+ Copyright (C) 2010  Anders Sigfridsson
+ 
+ TiY (tm) is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ TiY (tm) is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with TiY (tm).  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
 //
 //  FCGraphHandleView.m
 //  Future2
 //
 //  Created by Anders Sigfridsson on 06/10/2010.
-//  Copyright 2010 University of Limerick. All rights reserved.
 //
 
 #import "FCGraphHandleView.h"
